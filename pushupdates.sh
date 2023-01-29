3a01fc91e4c3b9cebe040b55bf485aba6fd741cb
Filip Maelbrancke <filip@maelbrancke.net>
2023-01-29 15:47:42 +0100

    config files on 29-01-2023 15:47:42

--- pushupdates.sh
+++ pushupdates.sh
@@ -1,4 +1,6 @@
 cd /config
+git config --global user.name "Filip Maelbrancke"
+git config --global user.email filip@maelbrancke.net
 git add .
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 git push -u origin master
